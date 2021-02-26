11ee06a891b57ea04e3334bc986fca54e65c490e
Sergio Renato Aldana Alvarez <sergio.aldana@galileo.edu>
2021-02-26 17:45:10 -0600

    Funcionalidad y estilos de la calculadora

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ Mousetrap.bind('+', clickSuma);
 
 document.getElementById('botonUno').addEventListener('click', clickUno);
 document.getElementById('botonDos').addEventListener('click', clickDos);
+document.getElementById('botonTres').addEventListener('click', clickTres);
+document.getElementById('botonCuatro').addEventListener('click', clickCuatro);
+document.getElementById('botonCinco').addEventListener('click', clickCinco);
+document.getElementById('botonSeis').addEventListener('click', clickSeis);
+document.getElementById('botonSiete').addEventListener('click', clickSiete);
+document.getElementById('botonOcho').addEventListener('click', clickOcho);
+document.getElementById('botonNueve').addEventListener('click', clickNueve);
+document.getElementById('botonCero').addEventListener('click', clickCero);
+
+
+
+
 document.getElementById('suma').addEventListener('click', clickSuma);
 
 let actualElemento = document.getElementById('numeroActual');
